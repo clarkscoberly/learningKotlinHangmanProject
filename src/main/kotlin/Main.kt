c369c44f15a6a41c14c7a70150d33784b739c3ac
clarkscoberly <cob18002@byui.edu>
2022-05-19 15:07:26 -0600

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,100 @@
+// Welcome welcome!!
+
+// Create class
+class Word {
+    val userWord = mutableListOf("s", "h", "a", "l", "o", "m")
+    val guessedLetters = mutableListOf<String>()
+    // Returns word to be checked against the user's input
+
+//    private fun getWord(): String {
+//        return "shalom"
+//    }
+
+    // TODO make the list a class property/attribute to be checked against within this fun-ction
+    fun checkLetterToGuess(letter : String) : Boolean {
+        /* takes a String and returns a boolean if the letter is in a list */
+        if (letter in userWord && letter !in guessedLetters) {
+            guessedLetters.add(letter)
+            println("Congrats! that letter '$letter' is in the word. ")
+            return true
+
+        }
+        else {
+            return false
+        }
+    }
+}
+
+class Player {
+
+    // Properties of player:
+    // Lives to calculate game ending condition
+    var lives = 5
+
+    fun getUserLetter(): String {
+        // Gets User input
+        println("Guess a letter: ")
+        return readLine()!!
+
+        // Eventually pass this is so that the valid letters are removed if they have previously guessed that letter.
+        //    val validLetters = mutableListOf('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
+        //    // Catch error of not inputting a valid character
+        //    while (letter.toString() !in validLetters) {
+        //        println("You entered an invalid character.")
+        //        println("Guess a letter: ")
+        //        val letter = readLine()
+        //    }
+        //    println("You entered: $letter")
+    }
+
+}
+
+fun printGreeting() {
+    println("----------------------------------------------")
+    println("                  HANGMAN")
+    println("----------------------------------------------\n")
+    println("Rules: \n1. Enjoy Yourself\n2. Guess Letters\n\n")
+}
+
+fun main(args: Array<String>) {
+    // classes
+    val player = Player()
+    val word = Word()
+    var correct = true
+    val printable = mutableListOf("_", "_", "_", "_", "_", "_")
+
+    // variables/lists
+    var notDone = true
+
+    // Initial setup
+    printGreeting()
+
+
+    // Main game loop
+    while (notDone) {
+        // Retrieves a guess to be checked against the word selected.
+        correct = word.checkLetterToGuess(player.getUserLetter())
+
+        if (!correct) {
+            player.lives -= 1
+            println("You have ${player.lives} live/s left")
+        } else {
+
+        }
+        if (player.lives == 0) {
+            println("---------------Game Over---------------")
+            notDone = false
+        }
+        // print correctly guessed letter in the right spot to be shown to user.
+        for ( letter in word.guessedLetters ) {
+            var index = word.userWord.indexOf(letter)
+            printable[index] = letter
+        }
+        println(printable)
+        if ("_" !in printable ) {
+            notDone = false
+        }
+
+        }
+
+}
